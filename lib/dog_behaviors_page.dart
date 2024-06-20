382ee60bcce82e44ad9ea8ed6ad524e1ea1cd92c
Yvettecarlos <yccarlos@cpp.edu>
2024-06-19 18:02:29 -0700

    Made changes to dog behaviors page

--- lib/dog_behaviors_page.dart
+++ lib/dog_behaviors_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class DogBehaviorsPage extends StatefulWidget {
@@ -11,6 +12,36 @@ class DogBehaviorsPage extends StatefulWidget {
 
 class _DogBehaviorsPageState extends State<DogBehaviorsPage> {
 
+  List<String> imagePaths = [
+    'assets/dog1.png',
+    'assets/dog2.png',
+    'assets/dog3.png',
+   'assets/dog4.png',
+    'assets/dog5.png',
+    'assets/dog6.png',
+    'assets/dog7.png',
+    'assets/dog8.png',
+
+  ];
+
+  List<String> outputs = [
+    'Clicked on dog1',
+    'Clicked on dog2',
+    'Clicked on dog3',
+    'Clicked on dog4',
+    'Clicked on dog1',
+    'Clicked on dog2',
+    'Clicked on dog3',
+    'Clicked on dog4',
+
+  ];
+
+  void handleTap(int index){
+    if (kDebugMode) {
+      print(outputs[index]);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
 
@@ -23,28 +54,41 @@ class _DogBehaviorsPageState extends State<DogBehaviorsPage> {
       ),
       body:
       Padding(
-        padding: const EdgeInsets.only(top: 15.0),
-        child: GridView.builder(
-          itemCount: 8,
-          gridDelegate:
-          const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            mainAxisSpacing: 15.0,
-            crossAxisSpacing: 1.0,
-            childAspectRatio: 1.5,
-
-          ),
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.only(top:10.0, bottom: 10.0, left: 40.0, right: 40.0),
-              child: Container(
-                color: Colors.blue,
-                height: 50,
-                width: 50,
+        padding: const EdgeInsets.fromLTRB(50.0,40.0,50.0,50.0),
+          child: GridView.builder(
+            itemCount: imagePaths.length,
+            gridDelegate:
+            const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              mainAxisSpacing: 20.0,
+              crossAxisSpacing: 60.0,
+              childAspectRatio: 1.0,
+        
+            ),
+            itemBuilder: (context, index) {
+              return GestureDetector(
+              onTap: (){
+                handleTap(index);
+              },
+              child: Card(
+              elevation: 5.0,
+              shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5.0),
               ),
-            );
-          },
-        ),
+              child: ClipRRect(
+              borderRadius: BorderRadius.circular(5.0),
+              child: AspectRatio(
+              aspectRatio: 1.0,
+              child: Image.asset(
+                imagePaths[index],
+                fit: BoxFit.cover,
+              ),
+                ),
+              ),
+              ),
+              );
+            },
+          ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
