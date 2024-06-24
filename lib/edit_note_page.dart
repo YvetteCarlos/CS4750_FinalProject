15df7187d246f9d6e8d8d4f01ae0856dd73b3ce1
Yvettecarlos <yccarlos@cpp.edu>
2024-06-23 20:46:52 -0700

    finished majority of log page, made some changes to edit note page

--- lib/edit_note_page.dart
+++ lib/edit_note_page.dart
@@ -12,46 +12,34 @@ class EditNotePage extends StatefulWidget {
 }
 
 class _EditNotePageState extends State<EditNotePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
 
-    return Scaffold(
-      appBar: AppBar(
+    return const Scaffold(
+    /*  appBar: AppBar(
 
         backgroundColor: Theme.of(context).colorScheme.primary,
 
         title: Text(widget.title),
-      ),
+      ),*/
       body: Center(
 
         child: Column(
 
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have push the button this many times:',
-            ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'You have push the button this many times:',
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
+     /* floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ), */// This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
--- lib/log_page.dart
+++ lib/log_page.dart
@@ -11,45 +11,143 @@ class MyLogPage extends StatefulWidget {
 }
 
 class _MyLogPageState extends State<MyLogPage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-
-      _counter++;
-    });
-  }
-
+  final titleController = TextEditingController();
+  final bodyController = TextEditingController();
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
       appBar: AppBar(
 
-        backgroundColor: Theme.of(context).colorScheme.primary,
+        backgroundColor: Colors.white,
 
         title: Text(widget.title),
-      ),
-      body: Center(
-
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have push the button this many times:',
+        actions: [
+          Container(
+            alignment: Alignment.topRight,
+            margin: const EdgeInsets.only(right: 20.0),
+            child: ElevatedButton(
+            onPressed: () {
+            print('Button pressed');
+            },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blueAccent[700],
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(1.0),
+                ),
+              ),
+              child: const Text(
+                  'Add Note',
+                      style: TextStyle(
+                      color: Colors.black,
+                        fontSize: 24,
+              ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+        ],
+      ),
+      resizeToAvoidBottomInset: false ,
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Container(
+              width: 360.0,
+              height: 650.0,
+              decoration: BoxDecoration(
+                color: Colors.blue[100],
+              ),
+              padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment:CrossAxisAlignment.start,
+            children: [
+             Row(
+               crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text(
+                  'Title:',
+                  style: TextStyle(
+                    fontSize: 24,
+                  ),
+                ),
+                Expanded(
+                    child: TextFormField(
+                      style: const TextStyle(
+                        fontSize: 24,
+                      ),
+                      decoration: const InputDecoration(
+                        border: InputBorder.none,
+                      ),
+                    ),
+                ),
+              ],
+            ),
+              Row(
+                children: [
+                  const Text(
+                    'Date:',
+                    style: TextStyle(
+                      fontSize: 24,
+                    ),
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      style: const TextStyle(
+                        fontSize: 24,
+                      ),
+                      decoration: const InputDecoration(
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  const Text(
+                    'Dog Behavior:',
+                    style: TextStyle(
+                      fontSize: 24,
+                    ),
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      style: const TextStyle(
+                        fontSize: 24,
+                      ),
+                      decoration: const InputDecoration(
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  const Text(
+                    'Potential Causes:',
+                    style: TextStyle(
+                      fontSize: 24,
+                    ),
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      style: const TextStyle(
+                        fontSize: 24,
+                      ),
+                      decoration: const InputDecoration(
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+            ),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
